88400f1c7ed8d29997d7a9a623a67412637a37dd
longluu <longluucong1308@gmail.com>
2022-08-03 17:02:52 +0700

    remove env

--- frontend/server/configs/s3.conf.js
+++ frontend/server/configs/s3.conf.js
@@ -1,13 +1,9 @@
 const AWS = require("aws-sdk");
 
-const ID = "AKIA3S6P576Y4AQIJW4U";
-const SECRET = "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";
-const BUCKET_NAME = "anhquynh.name.vn";
-
 const s3Config = new AWS.S3({
-  accessKeyId: ID,
-  secretAccessKey: SECRET,
-  Bucket: BUCKET_NAME
+  accessKeyId: process.env.ID,
+  secretAccessKey: process.env.SECRET,
+  Bucket: process.env.BUCKET_NAME
 });
 
 module.exports = {BUCKET_NAME, s3Config}
